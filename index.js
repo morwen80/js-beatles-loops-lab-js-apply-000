fa0fe11b830149c331b44c144903fd7ae083d2ae
Valeria Ragonese <ribesnero80@gmail.com>
2018-06-30 22:45:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,20 +5,13 @@ const instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
 function theBeatlesPlay(musicians, instruments) {
   var array = []
   for (let i = 0; i < musicians.length; i++) {
-    array.push(`${musicians[i]} plays ${instruments[i]}.`);
+    array.push(`${musicians[i]} plays ${instruments[i]}`);
   }
   return array;
 }
 
-function johnLennonFacts(facts){
- var facts =  [
-  "He was the last Beatle to learn to drive",
-  "He was never a vegetarian",
-  "He was a choir boy and boy scout",
-  "He hated the sound of his own voice"
-];
-
-var array = [];
+function johnLennonFacts(fact){
+ const facts = [ ];
 var j = 0;
 while(array.length < facts.length) {
   array.push(facts[j] + "!!!");
@@ -28,14 +21,14 @@ while(array.length < facts.length) {
 
 
 
-function iLoveTheBeatles(number) {
-  var array = [];
-  do {
-    array.push("I love the Beatles!");
-    number += 1;
-  }
-
-  while(number < 15) {
-    return array;
-  }
-}
+// function iLoveTheBeatles(number) {
+//   var array = [];
+//   do {
+//     array.push("I love the Beatles!");
+//     number += 1;
+//   }
+// 
+//   while(number < 15) {
+//     return array;
+//   }
+// }
